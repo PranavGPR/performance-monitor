689901e3e24c696891e13bd214894d4544138737
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-07-21 19:39:33 +0530

    CPU details collected on node server

---
+++ nodeClient/index.js
@@ -0,0 +1,64 @@
+const os = require("os");
+
+function performanceData() {
+  return new Promise(async (resolve, reject) => {
+    const cpus = os.cpus();
+    const osType = os.type();
+    const uptime = os.uptime();
+    const freeMem = os.freemem();
+    const totalMem = os.totalmem();
+    const usedMem = totalMem - freeMem;
+    const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
+    const cpuModel = cpus[0].model;
+    const cpuSpeed = cpus[0].speed;
+    const numCores = cpus.length;
+
+    const cpuLoad = await getCpuLoad();
+    resolve({
+      freeMem,
+      totalMem,
+      usedMem,
+      memUsage,
+      osType,
+      uptime,
+      cpuModel,
+      numCores,
+      cpuSpeed,
+      cpuLoad,
+    });
+  });
+}
+
+function cpuAverage() {
+  const cpus = os.cpus();
+  let idleMs = 0;
+  let totalMs = 0;
+
+  cpus.forEach((core) => {
+    for (type in core.times) {
+      totalMs += core.times[type];
+    }
+
+    idleMs += core.times.idle;
+  });
+  return { idle: idleMs / cpus.length, total: totalMs / cpus.length };
+}
+
+function getCpuLoad() {
+  return new Promise((resolve, reject) => {
+    const start = cpuAverage();
+    setTimeout(() => {
+      const end = cpuAverage();
+      const idleDifference = end.idle - start.idle;
+      const totalDifference = end.total - start.total;
+
+      const percentageCpu =
+        100 - Math.floor((100 * idleDifference) / totalDifference);
+      resolve(percentageCpu);
+    });
+  }, 100);
+}
+
+performanceData().then((data) => {
+  console.log(data);
+});
